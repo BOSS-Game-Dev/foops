e04d0afa030ff12161a8fb653e9930f04eb7a009
Sean-Chen71 <138247543+SeanChen2@users.noreply.github.com>
2024-02-12 16:06:57 -0500

    Added Camera Movement

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private float mouseSensitivity;
+
+    private Vector2 mouseInput;
+    private Vector2 camRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        camRotation.x -= mouseInput.y;
+        camRotation.y += mouseInput.x;
+
+        camRotation.x = Mathf.Clamp(camRotation.x, -90f, 90f);
+
+        transform.rotation = Quaternion.Euler(camRotation.x, camRotation.y, 0);
+    }
+
+    public void GetMouseInput(InputAction.CallbackContext callbackContext)
+    {
+        mouseInput = callbackContext.ReadValue<Vector2>() * mouseSensitivity * 0.02f;
+    }
+}
