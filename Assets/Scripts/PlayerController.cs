f1a9750c60f1038eb5818ee63563eaec82e09d3e
Sean-Chen71 <138247543+SeanChen2@users.noreply.github.com>
2024-03-18 16:02:25 -0400

    Jumping + Parkour Platforms

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,6 +11,15 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float movementSpeed;
 
+    [SerializeField]
+    private float jumpForce;
+
+    [SerializeField]
+
+    private LayerMask whatIsGround;
+
+    private readonly float maxRaycastDist = 1.1f;
+
     private Vector2 input;
     private Vector3 direction;
 
@@ -32,10 +41,34 @@ public class PlayerController : MonoBehaviour
     {
         direction = orientation.forward * input.y + orientation.right * input.x;
         rb.AddForce(direction * movementSpeed);
+
+        SpeedControl();
     }
 
     public void GetInput(InputAction.CallbackContext callbackContext) 
     {
         input = callbackContext.ReadValue<Vector2>().normalized;
+    }
+
+    public void Jump(InputAction.CallbackContext callbackContext) {
+        if (callbackContext.performed) {
+            bool isGrounded = Physics.Raycast(transform.position, Vector3.down, maxRaycastDist, whatIsGround);
+            Debug.Log(isGrounded);
+
+            if (isGrounded) 
+                // Add jump force
+                rb.AddForce(Vector2.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private void SpeedControl() {
+        //Only care about the player's velocity in the x-z plane (flat)
+        Vector3 flatVelocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+
+        if (flatVelocity.magnitude > movementSpeed) {
+            //Limit the player's velocity to the same direction, but at the max speed magnitude
+            Vector3 limitedVelocity = flatVelocity.normalized * movementSpeed;
+            rb.velocity = new Vector3(limitedVelocity.x, rb.velocity.y, limitedVelocity.z);
+        }
     }
 }
