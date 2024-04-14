c03b9c04ebe272462be97c831108d0b9d1d50fd2
22email <22imgs21212222@gmail.com>
2024-04-13 22:09:50 -0400

    modified player speed & fixed things broken by merge

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,7 +15,6 @@ public class PlayerController : MonoBehaviour
     private float jumpForce;
 
     [SerializeField]
-
     private LayerMask whatIsGround;
 
     private readonly float maxRaycastDist = 1.1f;
@@ -24,48 +23,54 @@ public class PlayerController : MonoBehaviour
     private Vector3 direction;
 
     private Rigidbody rb;
-    
+
     // Start is called before the first frame update
     void Start()
-    { 
-        rb = GetComponent<Rigidbody>();   
+    {
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void Update() { }
 
     private void FixedUpdate()
     {
         direction = orientation.forward * input.y + orientation.right * input.x;
-        rb.AddForce(direction * movementSpeed);
+        rb.AddForce(direction * movementSpeed * 10f, ForceMode.Acceleration);
 
         SpeedControl();
     }
 
-    public void GetInput(InputAction.CallbackContext callbackContext) 
+    public void GetInput(InputAction.CallbackContext callbackContext)
     {
         input = callbackContext.ReadValue<Vector2>().normalized;
     }
 
-    public void Jump(InputAction.CallbackContext callbackContext) {
-        if (callbackContext.performed) {
-            bool isGrounded = Physics.Raycast(transform.position, Vector3.down, maxRaycastDist, whatIsGround);
+    public void Jump(InputAction.CallbackContext callbackContext)
+    {
+        if (callbackContext.performed)
+        {
+            bool isGrounded = Physics.Raycast(
+                transform.position,
+                Vector3.down,
+                maxRaycastDist,
+                whatIsGround
+            );
             Debug.Log(isGrounded);
 
-            if (isGrounded) 
+            if (isGrounded)
                 // Add jump force
                 rb.AddForce(Vector2.up * jumpForce, ForceMode.Impulse);
         }
     }
 
-    private void SpeedControl() {
+    private void SpeedControl()
+    {
         //Only care about the player's velocity in the x-z plane (flat)
         Vector3 flatVelocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
 
-        if (flatVelocity.magnitude > movementSpeed) {
+        if (flatVelocity.magnitude > movementSpeed)
+        {
             //Limit the player's velocity to the same direction, but at the max speed magnitude
             Vector3 limitedVelocity = flatVelocity.normalized * movementSpeed;
             rb.velocity = new Vector3(limitedVelocity.x, rb.velocity.y, limitedVelocity.z);
